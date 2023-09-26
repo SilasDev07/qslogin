a82c5d31d0180639e90331016a763f33607c088b
silaaasxz1 <143753926+SilasDev07@users.noreply.github.com>
2023-09-26 16:28:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function abrirMenu() {
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block'
+   
+    }
+
+
+    else {
+        itens.style.display = 'none'
+    }
+
+
+}
