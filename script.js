b25f19652f790effb43c6d33ddffe3c328dc523e
silaaasxz1 <143753926+SilasDev07@users.noreply.github.com>
2023-10-06 18:09:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('img')
+let nav = document.querySelector('nav')
+function aparece(){
+    nav.style.width='300px'
+    nav.style.height='100vh'
+}
+function fecha(){
+    nav.style.width='0px'
+}
+
+
+
