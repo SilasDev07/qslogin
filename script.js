c93333f48c38606dbeccf6f5d085731a98f5b550
silaaasxz1 <143753926+SilasDev07@users.noreply.github.com>
2023-10-24 16:54:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 let img = document.querySelector('img')
 let nav = document.querySelector('nav')
-function aparece(){
-    nav.style.width='300px'
-    nav.style.height='100vh'
+function aparece() {
+    nav.style.width = '300px'
+    nav.style.height = '100vh'
 }
-function fecha(){
-    nav.style.width='0px'
+function fecha() {
+    nav.style.width = '0px'
 }
 
 
+ let imagens = document.querySelectorAll('.image')
+ console.log(imagens)
 
+let flag = true;
+
+function mostraSenha() {
+    let input = document.getElementById('senha')
+    if (flag == true) {
+        imagens[0].style.display = 'none'
+        imagens[1].style.display = 'block'
+        input.type = 'text'
+        flag = false
+    } else {
+        imagens[0].style.display = 'block'
+        imagens[1].style.display = 'none'
+        input.type = 'password'
+        flag = true;
+    }
+}
